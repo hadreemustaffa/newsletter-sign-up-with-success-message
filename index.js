eaa98fbdac67780df5d31187efc9b167bffe7c67
drevkr <hadree.mustaffa@gmail.com>
2023-08-31 21:28:38 +0800

    finish validation, success page and dismiss message

---
+++ index.js
@@ -0,0 +1,40 @@
+const subscribe = () => {
+	const form = document.getElementById('form');
+
+	const validateEmail = () => {
+		const emailAddress = document.querySelector('.email');
+		const errorMessage = document.querySelector('.error-message');
+		const formSection = document.querySelector('.form-section');
+		const successMessage = document.querySelector('.success-message');
+		const dismissBtn = document.querySelector('#dismissBtn');
+
+		const emailPattern = new RegExp(emailAddress.pattern);
+		const testPattern = emailPattern.test(emailAddress.value);
+
+		if (testPattern === false) {
+			errorMessage.textContent = 'Valid email required';
+			emailAddress.classList.add('error');
+		} else {
+			errorMessage.textContent = '';
+			emailAddress.classList.remove('error');
+			formSection.style.display = 'none';
+			successMessage.style.display = 'flex';
+		}
+
+		dismissBtn.addEventListener('click', () => {
+			emailAddress.value = '';
+			formSection.style.display = 'flex';
+			successMessage.style.display = 'none';
+		});
+	};
+
+	form.setAttribute('novalidate', '');
+
+	form.addEventListener('submit', (event) => {
+		event.preventDefault();
+
+		validateEmail();
+	});
+};
+
+subscribe();
